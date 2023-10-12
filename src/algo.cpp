0fc51abc3e8daa7ee1a90f79b6b6897be7a88c4e
makiroll1125 <145974358+makiroll1125@users.noreply.github.com>
2023-10-12 23:53:37 +0800

    Revert "practice update 2"
    This reverts commit 1c43c334ddef582089be30e5646c108ef9da762e.

--- src/algo.cpp
+++ src/algo.cpp
@@ -8,7 +8,14 @@
   * @retval          the result of gcd
   */
  int gcd(int a, int b);
-
+ /**
+  * @brief           find the Least Common Multiple
+  * @param[in]       a: one of the integer
+  * @param[in]       b: one of the integer
+  * @return          int
+  * @retval          the result of lcm
+  */
+ int lcmm(int a,int b);
  /**
   * @brief           find the Greatest Common Divisor
   * @param[in]       a: one of the integer
@@ -26,3 +33,15 @@
      }
      return b; 
  }
+  /**
+  * @brief           find the Least Common Multiple
+  * @param[in]       a: one of the integer
+  * @param[in]       b: one of the integer
+  * @return          int
+  * @retval          the result of lcm
+  */
+ int lcmm(int a, int b)
+ {
+         return a/gcd(a,b)*b;
+ }
+ // code from "modify_algo"
